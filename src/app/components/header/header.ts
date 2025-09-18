ce74e7646396b28467ba0f00e2da71a3befd05a5
MiguelGarzon11 <miguelangelgarb@gmail.com>
2025-09-18 12:56:02 -0500

    Restructure directorys

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,34 +1,43 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header implements AfterViewInit {
+export class Header {
+  isDark = false; 
 
-  ngAfterViewInit() {
-    const switchInput = document.querySelector<HTMLInputElement>('[data-hs-theme-switch]');
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { 
+    if (isPlatformBrowser(this.platformId)) {
+      const theme = localStorage.getItem('hs_theme');
+      this.isDark = theme === 'dark';
+      this.applyTheme();
+    }
+  }
 
-    if (switchInput) {
-      switchInput.checked = document.documentElement.classList.contains('dark');
+  onThemeToggle() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDark = !this.isDark;
+      localStorage.setItem('hs_theme', this.isDark ? 'dark' : 'light');
+      this.applyTheme();
+    }
+  }
 
-      switchInput.addEventListener('change', () => {
-        const html = document.documentElement;
-        const isDark = html.classList.contains('dark');
-
-        if (isDark) {
-          html.classList.remove('dark');
-          localStorage.setItem('hs_theme', 'light');
-        } else {
-          html.classList.add('dark');
-          localStorage.setItem('hs_theme', 'dark');
-        }
-      });
+  private applyTheme() {
+  if (isPlatformBrowser(this.platformId)) {
+      const html = document.documentElement;
+      if (this.isDark) {
+        html.classList.add('dark');
+      } else {
+        html.classList.remove('dark');
+      }
     }
   }
 }
