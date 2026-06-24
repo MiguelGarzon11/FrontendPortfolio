5939a8e37c2b317554e290faf1588fe2a395e3d7
MiguelGarzon11 <miguelangelgarb@gmail.com>
2026-06-23 20:03:37 -0500

    Add Mobile Card

--- src/app/presentation/components/projects/projects.ts
+++ src/app/presentation/components/projects/projects.ts
@@ -3,12 +3,13 @@ import { Card } from '../../shared/components/card/card';
 import { ProjectDTO } from '@/app/domain/dto/projectDTO';
 import { TypePlatform } from '@/app/domain/dto/projectDTO';
 import { Label } from '../../shared/components/label/label';
+import { MobileCard } from '../../shared/components/mobile-card/mobile-card';
 
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [Card, Label],
+  imports: [Card, Label, MobileCard],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
@@ -17,7 +18,8 @@ export class Projects {
   public Platform = TypePlatform;
 
   projects: ProjectDTO[] = [
-    { title: 'Vaultly', description: 'Vaultly te permite registrar tus gastos, planificar tu dinero y visualizar todo con claridad en un solo lugar.', tech: ['astro', 'typescript', 'tailwind', 'aws', 'postgres'], url: 'vaultlyc.vercel.app', type: TypePlatform.Web, github: 'github.com/mgarzondev/Vaultly', tag: 'Personal finances, clear accounts, secure life' }
+    { title: 'Vaultly', description: 'Vaultly te permite registrar tus gastos, planificar tu dinero y visualizar todo con claridad en un solo lugar.', tech: ['astro', 'typescript', 'tailwind', 'aws', 'postgres'], url: 'vaultlyc.vercel.app', type: TypePlatform.Web, github: 'github.com/mgarzondev/Vaultly', tag: 'Personal finances, clear accounts, secure life' },
+    { title: 'Vaultly Mobile', description: 'Vaultly Mobile lleva el poder de Vaultly Web a tu celular. Automatiza tu gestión financiera y dile adiós al registro manual de tus ingresos y gastos.', tech: ['kotlin', 'typescript', 'tailwind'], url: 'miguelangelgarzon.vercel.app', type: TypePlatform.Android, github: 'github.com/mgarzondev/VaultlyMobile', tag: 'Tus finanzas en piloto automático.' },
   ]
 
   modalVisible: boolean = false;
---
+++ src/app/presentation/shared/components/mobile-card/mobile-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-card',
+  imports: [],
+  templateUrl: './mobile-card.html',
+  styleUrl: './mobile-card.css',
+})
+export class MobileCard {
+
+}
